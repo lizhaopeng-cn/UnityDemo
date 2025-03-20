8636be7285985c6bcb79f89a5f83a39092e35588
lizhaopeng <lizhaopeng.cn@gmail.com>
2025-03-20 15:37:34 +0800

    first commit

---
+++ Assets/scripe/Hello.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+        SpriteRenderer sr = this.gameObject.GetComponent<SpriteRenderer>();
+        sr.flipY = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Application.targetFrameRate = 60;
+        Debug.Log("Lzp：" + Time.deltaTime);
+        this.transform.Translate(0, 0.05f, 0);
+    }
+}
